57611e1380e7d489d62fee81d6a75e1f87214fbc
Derek <derex1987@gmail.com>
2026-04-11 21:41:11 -0500

    chore: startup derexxd credits

--- src/main/java/io/github/derec4/unmount/Unmount.java
+++ src/main/java/io/github/derec4/unmount/Unmount.java
@@ -1,5 +1,7 @@
 package io.github.derec4.unmount;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Unmount extends JavaPlugin {
@@ -34,6 +36,13 @@ public final class Unmount extends JavaPlugin {
         } else {
             getLogger().info("Minecart collision snowplow: disabled (enable it in config.yml)");
         }
+
+        Bukkit.getLogger().info("");
+        Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "  |_______|                             " +
+                "  ");
+        Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "  | Derex |     Unmount v" + getDescription().getVersion());
+        Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "  |_______|     Running on " + Bukkit.getName() + " - " + Bukkit.getVersion());
+        Bukkit.getLogger().info("");
     }
 
     @Override
